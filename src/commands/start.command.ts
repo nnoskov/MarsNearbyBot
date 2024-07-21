5061db0b4ae79ad1e78a927ed32f5d0e32cb6456
nnoskov <hhb@bk.ru>
2024-07-21 11:35:48 +0300

    project structure update

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -3,6 +3,7 @@ import { Command } from "./command.class";
 import { IBotContext } from "../context/context.interface";
 import { beginCell, toNano } from "ton-core";
 import qs from "qs";
+import { readFileSync } from "fs";
 
 export class StartCommand extends Command {
   constructor(bot: Telegraf<IBotContext>) {
@@ -10,27 +11,27 @@ export class StartCommand extends Command {
   }
 
   handle(): void {
-    this.bot.start((ctx) => {
-      console.log(ctx.session);
-      ctx.reply("Welcome to Mars Nearby bot!", {
-        reply_markup: {
-          keyboard: [["Increment by 5"], ["Deposit 0.01"], ["Withdrawal 0.02"]],
-        },
-      });
-      //   ctx.reply(
-      //     "Welcome to Mars Nearby bot!",
-      //     Markup.keyboard([
-      //       Markup.button.callback("Increment by 5", "increment"),
-      //       Markup.button.callback("Deposit 0.01", "deposit"),
-      //       Markup.button.callback("Withdrawal 0.02", "withdrawal"),
-      //     ])
-      //   );
+    this.bot.start(async (ctx) => {
+      console.log(ctx.session, ctx.message);
+      const text = `Hi ${
+        ctx.message.from.first_name ?? "User"
+      }! Welcome to Mars Nearby TON Smart Contract testing bot!`;
+      await ctx.reply(
+        text,
+        Markup.inlineKeyboard([
+          [
+            Markup.button.webApp("🧪 Start TWA", "https://marsnearby.fun"),
+            Markup.button.callback("🍩 Donate 0.01", "donate"),
+          ],
+          [Markup.button.callback("About", "about")],
+        ])
+      );
     });
 
     // Hears
     this.bot.hears("Increment by 5", (ctx) => {
       const msg_body = beginCell().storeUint(1, 32).storeUint(5, 32).endCell();
-      let link = `https://app.tonkeeper.com/transfer/${
+      const link = `https://app.tonkeeper.com/transfer/${
         process.env.SC_ADDRESS
       }?${qs.stringify({
         text: "Increment counter by 5",
@@ -50,11 +51,10 @@ export class StartCommand extends Command {
         },
       });
     });
-
     this.bot.hears("Deposit 0.01", (ctx) => {
       const msg_body = beginCell().storeUint(2, 32).endCell();
 
-      let link = `https://app.tonkeeper.com/transfer/${
+      const link = `https://app.tonkeeper.com/transfer/${
         process.env.SC_ADDRESS
       }?${qs.stringify({
         text: "Deposit 0.01 TON",
@@ -80,7 +80,7 @@ export class StartCommand extends Command {
         .storeCoins(toNano("0.02"))
         .endCell();
 
-      let link = `https://app.tonkeeper.com/transfer/${
+      const link = `https://app.tonkeeper.com/transfer/${
         process.env.SC_ADDRESS
       }?${qs.stringify({
         text: "Withdraw 0.02 TON",
@@ -104,7 +104,7 @@ export class StartCommand extends Command {
     // Actions
     this.bot.action("increment", (ctx) => {
       const msg_body = beginCell().storeUint(1, 32).storeUint(5, 32).endCell();
-      let link = `https://app.tonkeeper.com/transfer/${
+      const link = `https://app.tonkeeper.com/transfer/${
         process.env.SC_ADDRESS
       }?${qs.stringify({
         text: "Increment counter by 5",
@@ -123,6 +123,46 @@ export class StartCommand extends Command {
           ],
         },
       });
+    });
+
+    this.bot.action("donate", (ctx) => {
+      const msg_body = beginCell().storeUint(2, 32).endCell();
+      const link = `https://app.tonkeeper.com/transfer/${
+        process.env.SC_ADDRESS
+      }?${qs.stringify({
+        text: "Donate by 0.01 TON",
+        amount: toNano("0.01").toString(10),
+        bin: msg_body.toBoc({ idx: false }).toString("base64"),
+      })}`;
+      ctx.reply("To donation me 0.01 TON, please sign a transaction:", {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Sign transaction",
+                url: link,
+              },
+            ],
+          ],
+        },
+      });
+    });
+
+    this.bot.action("about", async (ctx) => {
+      const txtHeader = "Text Header";
+      let txtBody = "";
+      try {
+        txtBody = readFileSync("./public/data/text/about.htm", "utf8");
+      } catch (error) {
+        console.log(error);
+      }
+
+      await ctx.replyWithHTML(
+        `<b>${txtHeader}</b>\n${txtBody}`,
+        Markup.inlineKeyboard([
+          Markup.button.webApp("Start testing", "https://marsnearby.fun"),
+        ])
+      );
     });
   }
 }
